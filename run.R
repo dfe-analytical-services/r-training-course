e034b209e4f4b5f4fc7e2857209f075faf52d41e
Adam Robinson <adamrobinson361@gmail.com>
2018-03-01 22:14:50 +0000

    Converted to bookdown structure

--- run.R
+++
@@ -1,5 +0,0 @@
-# Run markdown to produce output ------------------------------------------
-rmarkdown::render("r_training.Rmd",
-                  output_file = "r_training.html",
-                  output_dir = "outputs")
-rm(list = ls())
